367cc95adc153db8b61b69ef0d4132cc2bd6d960
小聿 <wszbdmyj@gmail.com>
2025-10-15 14:49:01 +0800

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,60 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue'
+import PanelHistory from './components/PanelHistory.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <el-container>
+      <!-- 头部标题 -->
+      <el-header><AppHeader /></el-header>
+      <el-main>
+        <el-row>
+          <!-- 左侧控制面板 -->
+          <div class="left-panel">
+            <el-col :span="5">
+              <!-- 对话历史记录 -->
+              <PanelHistory />
+              <!-- 图层控制 -->
+              <PanelLayer />
+            </el-col>
+          </div>
+
+          <!-- 中间地图视图 -->
+          <el-col :span="12">
+            <MapView />
+          </el-col>
+          <!-- 右侧对话框 -->
+          <el-col :span="7">
+            <PanelChat />
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#app {
+  height: 100vh; /* 整个应用占满视口高度 */
+  display: flex;
+  flex-direction: column;
+}
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-main {
+  flex: 1;
+  overflow: hidden;
+}
+.left-panel {
+  padding: 10px;
+  background-color: #0d3a68;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,61 @@
+<script lang="ts" setup>
+import logoUrl from '../assets/logo.png'
+</script>
+<template>
+  <div class="header">
+    <div class="header-logo">
+      <el-avatar shape="square" :size="30" :src="logoUrl" />
+      <div class="logo-title">GeoMind</div>
+    </div>
+    <el-divider direction="vertical" />
+    <div class="header-content">桌面工作台</div>
+  </div>
+</template>
+<style scoped>
+.header {
+  display: flex;
+  height: 60px;
+  align-items: center;
+  background-color: #0d3a68; /* 深蓝背景，可根据需要调整 */
+  padding: 10px 20px;
+}
+
+.header-logo {
+  display: flex;
+  align-items: center;
+}
+
+
+::v-deep(.el-avatar) {
+  width: 40px;
+  height: 40px;
+
+}
+
+::v-deep(.el-avatar img) {
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  border-radius: 6px;
+}
+
+.logo-title {
+  font-size: 40px;
+  font-weight: bold;
+  margin-left: 10px;
+  background: linear-gradient(90deg, #00c9a7, #007cf0); /* 蓝绿渐变 */
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.header-content {
+  font-size: 34px;
+  color: #ffffff; /* 白色字体 */
+}
+
+::v-deep(.el-divider--vertical) {
+  border-left: 2px solid #ffffff;
+  height: 40px;
+  margin: 0 15px;
+}
+</style>
